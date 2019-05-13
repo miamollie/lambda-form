eff9109f742a7a8bc928aa4b50c2fd4aeaa5668e
Mia de Búrca <mia.deburca@99designs.com>
2019-05-13 16:13:23 +1000

    Add types for entry form

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,77 @@
-export interface EntryType {
-  organisation: string;
-  website: string;
+export type EntryType = Applicant & Organisation & RelationshipToNineNine;
+
+export interface Address {
+  street: string;
+  streetLineTwo: string;
+  city: string;
+  province: string;
+  zip: string;
+  country: string;
 }
+
+export interface Applicant {
+  role: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+}
+
+export interface Organisation {
+  name: string;
+  streetAddress: string;
+  streetAddressTwo?: string;
+  city: string;
+  province: string;
+  zip: string;
+  country: string;
+  website: string;
+  foundationYear: string;
+  registrationStatus: boolean;
+  taxId: string;
+  localisation: Localisation;
+  hasParentAffiliation: boolean;
+  sector: Sector;
+  socialMediaHandle: string;
+  socialMediaFollowers: string;
+  description: string;
+}
+
+export interface RelationshipToNineNine {
+  priorUse: boolean;
+  contestType: AvailableContestTypes;
+  deadline: string;
+  specificEvent: boolean;
+  howDidYouHear: ReferralType;
+  featureAgreement: boolean;
+  collectEmailAgreement: boolean;
+  privacyAgreement: boolean;
+}
+
+type ReferralType =
+  | "internet"
+  | "word_of_mouth"
+  | "media"
+  | "blog"
+  | "social_media"
+  | "other";
+type AvailableContestTypes =
+  | "logo"
+  | "web_page"
+  | "tshirt"
+  | "social_page"
+  | "poster"
+  | "other_print_design"
+  | "unsure";
+type Localisation = "local" | "national" | "international";
+type Sector =
+  | "animal_welfare"
+  | "arts_culture"
+  | "civic_services"
+  | "environmental_advocacy"
+  | "education"
+  | "international_relations"
+  | "health_services"
+  | "religion"
+  | "social_legal"
+  | "other";
